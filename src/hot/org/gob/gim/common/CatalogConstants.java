81ce28f84ac504d968e66c08f4753b999933855d
Rene Ortega <renysh_12007@hotmail.com>
2016-08-09 10:00:59 -0500

    SUbiendo cambios de merge de Constantes de Catalogos

---
+++ src/hot/org/gob/gim/common/CatalogConstants.java
@@ -0,0 +1,20 @@
+package org.gob.gim.common;
+
+public class CatalogConstants {
+	
+	/*
+	 * CATALOGOS E ITEMS CATALOGO CODIGOS
+	 */
+	 public static final String CATALOG_STATES_IMPUGNMENT = "STATES_IMPUGNME";
+	 
+	 public static final String CATALOG_TYPES_IMPUGNMENTS = "TYPES_IMPUGNMEN";
+	 
+	 public static final String CATALOG_TYPES_TREATMENT_EXCEMPTION = "TYPES_TREATMENT_EXCEMPTION";
+	 
+	 public static final String ITEM_CATALOG_STATE_IMPUGNMENT_REGISTER = "STATE_REGISTE";
+	 
+	 public static final String ITEM_CATALOG_TYPE_IMPUGNMENT_FOTO_MULTA = "TYPE_IMP_FOTO_M";
+	 
+	 public static final String ITEM_CATALOG_TYPE_EXCLUDE_TREATMENT_EXCEMPTION = "TYPE_EXCLUDE";
+	
+}
