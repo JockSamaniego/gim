099cd75688f3738765f3820af4599b9a53d105c4
Jock Samaniego <jocksamaniego@gmail.com>
2018-12-10 10:41:12 -0500

    endoso multiple

--- src/main/ec/gob/gim/income/model/CreditNote.java
+++ src/main/ec/gob/gim/income/model/CreditNote.java
@@ -77,6 +77,7 @@ public class CreditNote {
 	@JoinColumn(name="creditNoteType_id")
 	private CreditNoteType creditNoteType;
 	
+	private Long parentCreditNote_id;
 	
 	public CreditNote() {
 		paymentFractions = new ArrayList<PaymentFraction>();
@@ -240,6 +241,14 @@ public class CreditNote {
 
 	public void setAvailableAmount(BigDecimal availableAmount) {
 		this.availableAmount = availableAmount;
+	}
+
+	public Long getParentCreditNote_id() {
+		return parentCreditNote_id;
+	}
+
+	public void setParentCreditNote_id(Long parentCreditNote_id) {
+		this.parentCreditNote_id = parentCreditNote_id;
 	}
 
 
