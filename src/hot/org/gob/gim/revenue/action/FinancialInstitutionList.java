628ca721d85d7f8cf15dc3f45d4bc49b935a56ec
Ronald Paladines C <rgpaladines@gmail.com>
2022-08-03 08:31:02 -0500

    corrección en busqueda de entidad financiera

--- src/hot/org/gob/gim/revenue/action/FinancialInstitutionList.java
+++ src/hot/org/gob/gim/revenue/action/FinancialInstitutionList.java
@@ -12,31 +12,42 @@ public class FinancialInstitutionList extends EntityQuery<FinancialInstitution> 
 
 	private static final long serialVersionUID = 4724358314849069961L;
 
-	private static final String EJBQL = "select financialInstitution from FinancialInstitution financialInstitution " +
-			"order by financialInstitution.name";
+	private static final String EJBQL = "select financialInstitution from FinancialInstitution financialInstitution ";
 
 	private static final String[] RESTRICTIONS = {
-			"lower(financialInstitution.name) like lower(concat('%',#{financialInstitutionList.financialInstitution.name},'%'))",};
+			"lower(financialInstitution.name) like lower(concat('%',#{financialInstitutionList.name},'%'))",};
 
 	private FinancialInstitution financialInstitution = new FinancialInstitution();
 	
-	
+	private String name;
 
 	public FinancialInstitutionList() {
 		setEjbql(EJBQL);
 		setRestrictionExpressionStrings(Arrays.asList(RESTRICTIONS));
+		setOrder(name);
 		setMaxResults(25);
 	}
-
-
 
 	public void setFinancialInstitution(FinancialInstitution financialInstitution) {
 		this.financialInstitution = financialInstitution;
 	}
 
-
-
 	public FinancialInstitution getFinancialInstitution() {
 		return financialInstitution;
 	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
