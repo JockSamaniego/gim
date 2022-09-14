fb8e5df6215e63ea415abbcb5c21a426812ea20f
Ronald Paladines C <rgpaladines@gmail.com>
2022-09-14 10:39:01 -0500

    Primera revision de calculo avaluo ordenanza 043

--- src/hot/org/gob/gim/appraisal/facade/AppraisalServiceBean.java
+++ src/hot/org/gob/gim/appraisal/facade/AppraisalServiceBean.java
@@ -391,6 +391,19 @@ public class AppraisalServiceBean implements AppraisalService {
 	public List<Property> calculateUrbanAppraisal(
 			AppraisalPeriod appraisalPeriod, int anioAppraisal,
 			List<Property> properties, boolean temporalValues) {
+		if (anioAppraisal > 2022){
+			return calculateUrbanAppraisal_Ordenanza043_2021(appraisalPeriod, anioAppraisal,
+					properties, temporalValues);
+		} else if (anioAppraisal <= 2022){
+			return calculateUrbanAppraisal_previous2022(appraisalPeriod, anioAppraisal,
+					properties, temporalValues);
+		} else 
+			return properties;
+	}
+
+	public List<Property> calculateUrbanAppraisal_previous2022(
+			AppraisalPeriod appraisalPeriod, int anioAppraisal,
+			List<Property> properties, boolean temporalValues) {
 		BigDecimal affectationFactorLot = new BigDecimal(0);
 		BigDecimal lotAppraisal = new BigDecimal(0);
 
@@ -694,25 +707,334 @@ public class AppraisalServiceBean implements AppraisalService {
 					RoundingMode.HALF_UP);
 
 			if (temporalValues) {
-				// BigDecimal aditionalValue = BigDecimal.ZERO;
-				// if (anioAppraisal == 2015){
-				// aditionalValue = totalBuildingAppraisal.multiply(new
-				// BigDecimal(18.8)).divide(cienBigD, RoundingMode.HALF_UP);
-				// }
-				// totalBuildingAppraisal =
-				// totalBuildingAppraisal.add(aditionalValue);
 				property.getCurrentDomain().setBuildingAppraisalTmp(
 						totalBuildingAppraisal);
 				property.getCurrentDomain().setCommercialAppraisalTmp(
 						totalBuildingAppraisal.add(lotAppraisal));
 			} else {
-				// BigDecimal aditionalValue = BigDecimal.ZERO;
-				// if (anioAppraisal == 2015){
-				// aditionalValue = totalBuildingAppraisal.multiply(new
-				// BigDecimal(18.8)).divide(cienBigD, RoundingMode.HALF_UP);
-				// }
-				// totalBuildingAppraisal =
-				// totalBuildingAppraisal.add(aditionalValue);
+				property.getCurrentDomain().setBuildingAppraisal(
+						totalBuildingAppraisal);
+				property.getCurrentDomain().setCommercialAppraisal(
+						totalBuildingAppraisal.add(lotAppraisal));
+			}
+		}
+
+		return properties;
+
+	}
+
+    //Cálculo de Acuerdo a la Ordenanza 043-2021
+	public List<Property> calculateUrbanAppraisal_Ordenanza043_2021(
+			AppraisalPeriod appraisalPeriod, int anioAppraisal,
+			List<Property> properties, boolean temporalValues) {
+		BigDecimal affectationFactorLot = new BigDecimal(0);
+		BigDecimal lotAppraisal = new BigDecimal(0);
+
+		BigDecimal affectationFactorBuilding = new BigDecimal(0);
+		BigDecimal buildingAppraisal = new BigDecimal(0);
+		BigDecimal totalBuildingAppraisal = new BigDecimal(0);
+		BigDecimal cienBigD = new BigDecimal(100);
+		// BigDecimal ceroBigD = BigDecimal.ZERO;
+
+		getMaps(appraisalPeriod);
+
+		/*
+		 * Consultar factores de correccion bienio 2020-2021
+		 */
+		affectationFactors = getAffectationFactors(appraisalPeriod);
+
+		if (systemParameterService == null)
+			systemParameterService = ServiceLocator.getInstance().findResource(
+					SYSTEM_PARAMETER_SERVICE_NAME);
+		/*
+		 * CODIGOS DE PARROQUIAS URBANAS
+		 */
+		String strListParishesUrbans = systemParameterService
+				.findParameter("URBAN_PARISH_CODES");
+		List<String> parishesUrbansListStrings = new ArrayList<String>();
+		parishesUrbansListStrings = GimUtils
+				.convertStringWithCommaToListString(strListParishesUrbans);
+
+		/*
+		 * CODIGOS DE PARROQUIAS URBANAS PARROQUIALES
+		 */
+		String strListParishesUrbanParishes = systemParameterService
+				.findParameter("URBANPARISH_PARISH_CODES");
+		List<String> parishesUrbanParishesListStrings = new ArrayList<String>();
+		parishesUrbanParishesListStrings = GimUtils
+				.convertStringWithCommaToListString(strListParishesUrbanParishes);
+
+		for (Property property : properties) {
+			// Inicia Calculo de Avaluo de Terreno
+			// System.out.println("======= CadastralCode: "
+			// + property.getCadastralCode());
+			lotAppraisal = BigDecimal.ZERO;
+			affectationFactorLot = BigDecimal.ZERO;
+			property.setAppraisalRelationFactor(getAppraisalRelationFactor(property));
+			// System.out.println("======= RelationFactor: "+property.getAppraisalRelationFactor());
+
+			property.setAppraisalAreaFactor(getAppraisalAreaFactor(property));
+			// System.out.println("======= AreaFactor: "+property.getAppraisalAreaFactor());
+
+			affectationFactorLot = property.getAppraisalRelationFactor()
+					.multiply(property.getAppraisalAreaFactor());
+			
+			// System.out.println("======= AppraisalAreaFactor: "+affectationFactorLot);
+			
+			// System.out.println("======= LotPosition: "+property.getLotPosition().getFactor());
+			affectationFactorLot = affectationFactorLot.multiply(property
+					.getLotPosition().getFactor());
+			
+			// System.out.println("======= affectationFactorLot: "+affectationFactorLot);
+			// System.out.println("======= Lottopography: "+property.getLotTopography().getFactor());
+			affectationFactorLot = affectationFactorLot.multiply(property
+					.getLotTopography().getFactor());
+			
+			// System.out.println("======= affectationFactorLot: "+affectationFactorLot);
+			// System.out.println("======= StreetMaterial: "+property.getStreetMaterial().getFactor());
+			affectationFactorLot = affectationFactorLot.multiply(property
+					.getStreetMaterial().getFactor());
+			// System.out.println("======= affectationFactorLot: "+affectationFactorLot);
+
+			// INFRAESTRUCTURA
+			if (property.getHasWaterService()){
+				affectationFactorLot = affectationFactorLot
+						.multiply(appraisalPeriod.getFactorHasWater()); 
+				// System.out.println("======= WaterService: "+appraisalPeriod.getFactorHasWater());
+			}else{
+				affectationFactorLot = affectationFactorLot
+						.multiply(appraisalPeriod.getFactorHasntWater()); 
+				// System.out.println("======= WaterService: "+appraisalPeriod.getFactorHasntWater());
+			}
+
+			if (property.getSewerage().compareTo(Sewerage.NOT_AVAILABLE) == 0){
+				affectationFactorLot = affectationFactorLot
+						.multiply(appraisalPeriod.getFactorHasntSewerage());
+				// System.out.println("======= Sewerage: "+appraisalPeriod.getFactorHasntSewerage());
+			}else{
+				affectationFactorLot = affectationFactorLot
+						.multiply(appraisalPeriod.getFactorHasSewerage());
+				// System.out.println("======= Sewerage: "+appraisalPeriod.getFactorHasSewerage());
+			}
+			
+			if (property.getHasElectricity()){
+				affectationFactorLot = affectationFactorLot
+						.multiply(appraisalPeriod.getFactorHasEnergy());
+				// System.out.println("======= Energy: "+appraisalPeriod.getFactorHasEnergy());
+			}else{
+				affectationFactorLot = affectationFactorLot
+						.multiply(appraisalPeriod.getFactorHasntEnergy());
+				// System.out.println("======= Energy: "+appraisalPeriod.getFactorHasntEnergy());
+			}
+			
+			/*
+			 * Calculo con nuevos factores de correccion bienio 2020-2021
+			 * 2019-12-02 rortega
+			 */
+
+			String _parish = property.getCadastralCode().substring(4, 6);
+
+			if (parishesUrbansListStrings.contains(_parish)) {
+				property.setAppraisalRiskFactor(getAppraisalRiskFactor(property));
+				property.setAppraisalThreatFactor(BigDecimal.ONE);
+			} else if (parishesUrbanParishesListStrings.contains(_parish)) {
+				property.setAppraisalRiskFactor(BigDecimal.ONE);
+				property.setAppraisalThreatFactor(getAppraisalThreatFactor(property));
+			} else {
+				System.out
+						.println("NO SE ENCUENTRA PARROQUIA, POSIBLE CLAVE SIN MIGRACION");
+			}
+			
+			// System.out.println("======= RiskFactor: "+property.getAppraisalRiskFactor());
+			// System.out.println("======= ThreatFactor: "+property.getAppraisalThreatFactor());
+
+			// Riesgo
+			affectationFactorLot = affectationFactorLot.multiply(property
+					.getAppraisalRiskFactor());
+
+			// Amenaza
+			affectationFactorLot = affectationFactorLot.multiply(property
+					.getAppraisalThreatFactor());
+			
+			// Acera
+			affectationFactorLot = affectationFactorLot
+					.multiply(getAppraisalSidewalkFactor(property));
+			
+			// System.out.println("======= SidewalkFactor: "+getAppraisalSidewalkFactor(property));
+			
+			// Bordillo
+			affectationFactorLot = affectationFactorLot
+					.multiply(getAppraisalCurbFactor(property));
+			
+			// System.out.println("======= CurbFactor: "+getAppraisalCurbFactor(property));
+			
+			// Recoleccion de basura
+			affectationFactorLot = affectationFactorLot
+					.multiply(getAppraisalGarbageCollectionFactor(property));
+			
+			// System.out.println("======= lGarbageCollection: "+getAppraisalGarbageCollectionFactor(property));
+
+			affectationFactorLot = affectationFactorLot
+					.round(new MathContext(4));
+			property.setAffectationFactorLot(affectationFactorLot);
+			
+			// System.out.println("======= affectationFactorLot: "+affectationFactorLot);
+
+			if (!temporalValues) {
+				lotAppraisal = property.getCurrentDomain()
+						.getValueBySquareMeter()
+						.multiply(property.getAffectationFactorLot());
+				lotAppraisal = lotAppraisal.multiply(property.getArea());
+				lotAppraisal = lotAppraisal.setScale(2, RoundingMode.HALF_UP);
+			} else {
+				lotAppraisal = property.getCurrentDomain()
+						.getValueBySquareMeterTmp()
+						.multiply(property.getAffectationFactorLot());
+				lotAppraisal = lotAppraisal.multiply(property.getArea());
+				lotAppraisal = lotAppraisal.setScale(2, RoundingMode.HALF_UP);
+			}
+
+			// System.out.println("======= totalLotAppraisal: " + lotAppraisal);
+			if ((property.getLotAliquot().floatValue() > 0)
+					&& (property.getLotAliquot().floatValue() < 100)) {
+				lotAppraisal = lotAppraisal.multiply(property.getLotAliquot())
+						.divide(cienBigD);
+				lotAppraisal = lotAppraisal.setScale(2, RoundingMode.HALF_UP);
+				/*
+				 * System.out.println("======= Lot Aliquot: " +
+				 * property.getLotAliquot());
+				 * System.out.println("======= totalLotAppraisal Aliquot: " +
+				 * lotAppraisal);
+				 */
+			}
+
+			if (temporalValues) {
+				property.getCurrentDomain().setLotAppraisalTmp(lotAppraisal);
+			} else {
+				property.getCurrentDomain().setLotAppraisal(lotAppraisal);
+			}
+
+			// System.out.println("======= lotAppraisal : " + lotAppraisal);
+
+			// Inicia Calculo de Avaluo de Construccion
+			totalBuildingAppraisal = BigDecimal.ZERO;
+			for (Building building : property.getBuildings()) {
+				// System.out.println("======= Construction: "+building.getId());
+				buildingAppraisal = BigDecimal.ZERO;
+				affectationFactorBuilding = BigDecimal.ZERO;
+				affectationFactorBuilding = affectationFactorBuilding
+						.add(mapTotalStructure.get(
+								building.getStructureMaterial().name())
+								.getTotal());
+				affectationFactorBuilding = affectationFactorBuilding
+						.add(mapTotalWall
+								.get(building.getWallMaterial().name())
+								.getTotal());
+				affectationFactorBuilding = affectationFactorBuilding
+						.add(mapTotalRoof
+								.get(building.getRoofMaterial().name())
+								.getTotal());
+				affectationFactorBuilding = affectationFactorBuilding
+						.add(mapTotalExternal.get(
+								building.getExternalFinishing().name())
+								.getTotal());
+				// System.out.println("======= Structure: "+mapTotalStructure.get(building.getStructureMaterial().name()));
+				// System.out.println("======= Structure: "+mapTotalStructure.get(building.getStructureMaterial().name()).getTotal());
+				// System.out.println("======= Wall: "+mapTotalWall.get(building.getWallMaterial().name()));
+				// System.out.println("======= Wall: "+mapTotalWall.get(building.getWallMaterial().name()).getTotal());
+				// System.out.println("======= Roof: "+mapTotalRoof.get(building.getRoofMaterial().name()));
+				// System.out.println("======= Roof: "+mapTotalRoof.get(building.getRoofMaterial().name()).getTotal());
+				// System.out.println("======= External: "+mapTotalExternal.get(building.getExternalFinishing().name()));
+				// System.out.println("======= External: "+mapTotalExternal.get(building.getExternalFinishing().name()).getTotal());
+				//
+				// System.out.println("======= affectationFactorBuilding: "+affectationFactorBuilding);
+
+				// Equipment
+				if (building.getHasEquipment()) {
+					affectationFactorBuilding = affectationFactorBuilding
+							.multiply(appraisalPeriod.getFactorHasEquipment());
+					// System.out.println("======= appraisalPeriod.getFactorHasEquipment(): "+appraisalPeriod.getFactorHasEquipment());
+				} else {
+					affectationFactorBuilding = affectationFactorBuilding
+							.multiply(appraisalPeriod.getFactorHasntEquipment());
+					// System.out.println("======= appraisalPeriod.getFactorHasntEquipment(): "+appraisalPeriod.getFactorHasntEquipment());
+				}
+
+				// System.out.println("======= affectationFactorBuilding: "+affectationFactorBuilding);
+
+				// preservationState
+				Long anioConst = anioAppraisal
+						- building.getBuildingYear().longValue();// building.getAnioConst().longValue();
+				if (anioConst > 99)
+					anioConst = Long.valueOf(99);
+				BigDecimal factorRoss = new BigDecimal(1);
+				// System.out.println("======= Años: "+anioConst);
+				// System.out.println("======= PreservationState: "+building.getPreservationState().name());
+				if (building.getPreservationState().compareTo(
+						PreservationState.GOOD) == 0) {
+					factorRoss = mapRossHeidecke.get(anioConst).getGoodState();
+				} else if (building.getPreservationState().compareTo(
+						PreservationState.BAD) == 0) {
+					factorRoss = mapRossHeidecke.get(anioConst).getBadState();
+				} else if (building.getPreservationState().compareTo(
+						PreservationState.REGULAR) == 0) {
+					factorRoss = mapRossHeidecke.get(anioConst)
+							.getRegularState();
+				}
+
+				// System.out.println("======= factorRoss : "+factorRoss );
+
+				factorRoss = factorRoss.divide(new BigDecimal(100));
+
+				// System.out.println("======= factorRoss : "+factorRoss );
+
+				BigDecimal substract = new BigDecimal(0);
+				// equivale al 80% --> (VA-Vr) --> Vr = 20% de VA
+				substract = affectationFactorBuilding.multiply(new BigDecimal(
+						0.8));
+				substract = substract.multiply(factorRoss);
+
+				// System.out.println("======= substract: "+substract);
+
+				// valor por m2 de edificacion para esa porcion de la
+				// construccion
+				affectationFactorBuilding = affectationFactorBuilding
+						.subtract(substract);
+
+				buildingAppraisal = building.getArea().multiply(
+						affectationFactorBuilding);
+				buildingAppraisal = buildingAppraisal.multiply(BigDecimal
+						.valueOf(building.getFloorsNumber()));
+
+				// System.out.println("======= Area: "+building.getArea());
+				// System.out.println("======= Pisos: "+building.getFloorsNumber());
+				// System.out.println("======= affectationFactorBuilding: "+affectationFactorBuilding);
+
+				totalBuildingAppraisal = totalBuildingAppraisal
+						.add(buildingAppraisal);
+				// System.out.println("======= totalBuildingAppraisal : " +
+				// totalBuildingAppraisal);
+			}
+
+			if ((property.getBuildingAliquot().floatValue() >= 0)
+					&& (property.getBuildingAliquot().floatValue() < 100)) {
+				totalBuildingAppraisal = totalBuildingAppraisal.multiply(
+						property.getBuildingAliquot()).divide(cienBigD);
+				// System.out.println("======= Building Aliquot: " +
+				// property.getBuildingAliquot());
+				// System.out.println("======= totalBuildingAppraisal Aliquot: "
+				// + totalBuildingAppraisal);
+			}
+
+			totalBuildingAppraisal = totalBuildingAppraisal.setScale(2,
+					RoundingMode.HALF_UP);
+
+			if (temporalValues) {
+				property.getCurrentDomain().setBuildingAppraisalTmp(
+						totalBuildingAppraisal);
+				property.getCurrentDomain().setCommercialAppraisalTmp(
+						totalBuildingAppraisal.add(lotAppraisal));
+			} else {
 				property.getCurrentDomain().setBuildingAppraisal(
 						totalBuildingAppraisal);
 				property.getCurrentDomain().setCommercialAppraisal(
--- src/main/ec/gob/gim/cadaster/model/LotTopography.java
+++ src/main/ec/gob/gim/cadaster/model/LotTopography.java
@@ -8,15 +8,17 @@ import java.math.BigDecimal;
  * @created 04-Ago-2011 16:30:38
  */
 public enum LotTopography {
-	IN_LEVEL(new BigDecimal(1.0)),
-	UNDER_LEVEL(new BigDecimal(0.95)),
-	ABOVE_LEVEL(new BigDecimal(0.98)),
-	PRECIPITOUS(new BigDecimal(0.75));
+	IN_LEVEL(new BigDecimal(1.0), new BigDecimal(1.0)),
+	UNDER_LEVEL(new BigDecimal(0.95), new BigDecimal(1.0)),
+	ABOVE_LEVEL(new BigDecimal(0.98), new BigDecimal(1.0)),
+	PRECIPITOUS(new BigDecimal(0.75), new BigDecimal(1.0));
 	
 	BigDecimal factor;
+	BigDecimal factorOrd43_2021; // Ordenanza 43-2021
 	
-	private LotTopography(BigDecimal factor) {
+	private LotTopography(BigDecimal factor, BigDecimal factorOrd43_2021) {
 		this.factor = factor;
+		this.factorOrd43_2021 = factorOrd43_2021;
 	}
 
 	public BigDecimal getFactor() {
@@ -25,6 +27,14 @@ public enum LotTopography {
 
 	public void setFactor(BigDecimal factor) {
 		this.factor = factor;
+	}
+
+	public BigDecimal getFactorOrd43_2021() {
+		return factorOrd43_2021;
+	}
+
+	public void setFactorOrd43_2021(BigDecimal factorOrd43_2021) {
+		this.factorOrd43_2021 = factorOrd43_2021;
 	}
 
 }
